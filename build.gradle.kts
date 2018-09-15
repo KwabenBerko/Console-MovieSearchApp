79c06a9f5a94dd3a99bdac41ee252c0dec4023d5
KwabenBerko <kwabenaberko95@gmail.com>
2018-09-15 22:53:40 +0000

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.51"
+}
+
+group = "com.kwabenaberko"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    implementation("io.reactivex.rxjava2:rxkotlin:2.3.0")
+    implementation("com.squareup.retrofit2:retrofit:2.4.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
